0374f6960a565b393b44d4d9089d11fb6ac0b906
javier <juli99nic@gmail.com>
2020-03-24 19:11:42 -0300

    tipos de datos

---
+++ Fundamentos/tipos-datos.py
@@ -0,0 +1,8 @@
+x=5  #int
+y=10.1 #float
+z=True #Bool
+print(x)    # id(var) muestra la direccion de memoria, solo funca en python, no vs
+print(y)
+print(z)    # type(var) muestra el tipo de variable. Las variables toman el tipo del valor que se les inicializa. funca en py
+a="Texto de ejemplo"
+print(a)
