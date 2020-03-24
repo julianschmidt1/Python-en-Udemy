657163c67f2fd35f4a37d96e5963f0ce629a0010
javier <juli99nic@gmail.com>
2020-03-24 20:52:18 -0300

    tipo string

---
+++ Fundamentos/tipo-string.py
@@ -0,0 +1,11 @@
+cadena="Hola mundo"
+print("Las primeras palabras de un programador son: "+cadena)
+
+numeroUno="1"
+numeroDos="2"
+print("Concatenacion: "+numeroUno+numeroDos)
+
+num1=1
+num2=2
+print("Operacion suma: "+ str(num1+num2))
+print("Operacion suma: ",num1+num2)
