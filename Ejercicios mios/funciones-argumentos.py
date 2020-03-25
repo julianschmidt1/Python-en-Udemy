f8d536a15569d7ef775d8ef2bda3bb89867d5afa
javier <juli99nic@gmail.com>
2020-03-25 15:10:46 -0300

    Funciones con argumentos

---
+++ Ejercicios mios/funciones-argumentos.py
@@ -0,0 +1,6 @@
+#Parametro: variable definida entre parentesis de la definicion de la funcion
+#arg: Valor enviado a la funcion
+def funcion_arg(nombre, apellido):
+    print("El nombre recibido es:", nombre+" "+apellido)
+    
+funcion_arg("Julian", "Schmidt")
