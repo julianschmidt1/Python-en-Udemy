35827bac9c4e4c39ffaf1f1b0b35601a2e6d83f9
javier <juli99nic@gmail.com>
2020-03-25 12:28:18 -0300

    LISTAS add, del

--- Fundamentos/lista.py
+++ Fundamentos/lista.py
@@ -1,4 +1,4 @@
-nombres=["Julian", "Javier", "Guillermo", "Romina"]
+nombres=["Pablo", "Javier", "Guillermo", "Romina"]
 print(nombres)
 #largo de la lista
 print(len(nombres))
@@ -15,7 +15,7 @@ print(nombres[:3])
 #imprimir elementos hasta el final desde indice especifico
 print(nombres[1:])
 #cambiar elementos de una lista
-nombres[0]="Pablo"
+nombres[0]="Julian"
 print(nombres)
 #iterar la lista
 for nombre in nombres:
@@ -25,3 +25,31 @@ if "Julian" in nombres:
     print("Julian si existe en la lista")
 else:
     print("Julian no existe")
+    
+#agregar nuevo elemento
+nombres.append("Pepe")
+print(nombres)
+
+#agregar elementos en el indice especifico
+nombres.insert(4, "Bono")
+print(nombres)
+
+#eliminar elemento de lista
+nombres.remove("Pepe")
+print(nombres)
+
+#eliminar ultimo elemento
+nombres.pop()
+print(nombres)
+
+#eliminar elemento en indice especifico
+del nombres[1]
+print(nombres)
+
+#limpiar elementos de lista
+nombres.clear()
+print(nombres)
+
+#eliminar lista
+del nombres
+print(nombres)
