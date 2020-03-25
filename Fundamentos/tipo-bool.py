c82e937cccf65fca0c68c46e3ebcbd5e96b78a71
javier <juli99nic@gmail.com>
2020-03-24 21:03:53 -0300

    tipo bool

---
+++ Fundamentos/tipo-bool.py
@@ -0,0 +1,15 @@
+x=True
+print(x)
+
+y=False
+print(y)
+
+num1=2
+num2=1
+resultado= num2<num1
+print(resultado)
+
+if(num1<num2):
+    print("El valor de num1 es menor que el valor de num2")
+else:
+    print("El valor de num1 no es menor que el valor de num2")
