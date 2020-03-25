0d4e0745f20d172459dd3bd95ebc25b13b15f452
javier <juli99nic@gmail.com>
2020-03-25 06:41:36 -0300

    ejercicio perimetro del rectangulo

---
+++ Fundamentos/ejercicio3.py
@@ -0,0 +1,10 @@
+print("En este ejercicio se calculara el area y perimetro de un rectangulo, ingresa los valores cuando se te pidan.")
+
+alto=int(input("Proporciona el alto del rectangulo: "))
+ancho=int(input("Proporciona el ancho del rectangulo: "))
+
+area=alto*ancho
+perimetro=(alto+ancho)*2
+
+print("El area del rectangulo es: ",area)
+print("El perimetro del rectangulo es: ",perimetro)
