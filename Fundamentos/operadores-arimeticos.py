27ae34fdd3b981a5595677047060f149d3c6dfcc
javier <juli99nic@gmail.com>
2020-03-25 05:43:40 -0300

    Operador de asignacion

---
+++ Fundamentos/operadores-arimeticos.py
@@ -0,0 +1,14 @@
+a=int(input("Ingrese el primer numero a operar: "))
+b=int(input("Ingrese el segundo numero a operar: "))
+suma=a+b
+print("La suma es: ", suma)
+resta=a-b
+print("La resta es: ", resta)
+multi=a*b
+print("La multiplicacion es: ", multi)
+division=a/b
+print("La division es: ", division)
+modulo=a%b
+print("El modulo es: ", modulo)
+exponente=a**b
+print("El exponente es: ", exponente)
