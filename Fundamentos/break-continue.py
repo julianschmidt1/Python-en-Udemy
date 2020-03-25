cf85c51cc15b7deb5bd7d7affed6966a3c115cfe
javier <juli99nic@gmail.com>
2020-03-25 11:14:26 -0300

    break y continue

---
+++ Fundamentos/break-continue.py
@@ -0,0 +1,15 @@
+for letra in "Abra Cadabra":
+    if letra == "a":
+        print(letra)
+        break
+
+#Imprimir pares
+#for i in range(6):
+#    if i%2==0:
+#        print(i)
+        
+#Imprimir pares continue
+for i in range(6):
+    if i%2 != 0:
+        continue
+    print(i)
