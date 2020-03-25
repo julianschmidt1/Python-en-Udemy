12cb64a5fd53a753d19ae9003acab6f5228a0fe3
javier <juli99nic@gmail.com>
2020-03-25 06:49:27 -0300

    ejercicio 4 numero mayor

---
+++ Fundamentos/ejercicio4.py
@@ -0,0 +1,7 @@
+primerNumero=int(input("Proporciona el numero1: "))
+segundoNumero=int(input("Proporciona el numero2: "))
+
+if (primerNumero>segundoNumero):
+    print("El numero mayor es: ",primerNumero)
+else:
+    print("El numero mayor es: ",segundoNumero)
