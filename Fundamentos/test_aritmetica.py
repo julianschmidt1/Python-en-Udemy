a253d3214075049ddf43770a76b2505878aa1919
javier <juli99nic@gmail.com>
2020-04-09 00:20:00 -0300

    Modulos python

---
+++ Fundamentos/test_aritmetica.py
@@ -0,0 +1,13 @@
+import modulos.modulo_aritmetica as aritmetica
+
+resultado = aritmetica.sumar(1,2)
+print(resultado)
+
+resultado=aritmetica.restar(3,1)
+print(resultado)
+
+resultado=aritmetica.multiplicar(2,2)
+print(resultado)
+
+resultado=aritmetica.dividir(10,5)
+print(resultado)
