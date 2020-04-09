83fd9d0b0f68dab7906dc122a7d29908c0598281
julianschmidt1 <juli99nic@gmail.com>
2020-04-09 15:39:17 -0300

    metodos-privados, publico, _protegido, __privado

--- Fundamentos/metodos-privados.py
+++ Fundamentos/metodos-privados.py
@@ -1,6 +1,7 @@
 class Persona:
-    def __init__(self, nombre, ape_paterno, ape_materno):
+    def __init__(self, nombre, nombre2, ape_paterno, ape_materno):
         self.nombre=nombre
+        self.__segundo_nombre=nombre2
         self._apellido_paterno=ape_paterno
         self.__apellido_materno=ape_materno
         
@@ -9,6 +10,7 @@ class Persona:
         
     def __metodo_privado(self):
         print(self.nombre)
+        print(self.__segundo_nombre)
         print(self._apellido_paterno)
         print(self.__apellido_materno)
         
@@ -17,10 +19,17 @@ class Persona:
     
     def set_apellido_materno(self, ape_materno):
         self.__apellido_materno=ape_materno
+        
+    def get_segundo_nombre(self):
+        return self.__segundo_nombre
     
-p1=Persona("Julian", "Schmidt", "Cipollari ")
+    def set_segundo_nombre(self, nombre2):
+        self.__segundo_nombre=nombre2
+    
+p1=Persona("Julian", "Nicolas", "Schmidt", "Cipollari ")
 p1.metodo_publico()
 
 print(p1.nombre)
+print(p1.get_segundo_nombre())
 print(p1._apellido_paterno)
 print(p1.get_apellido_materno())
