841afc53dfff7382d77281875ac8485386a66f1f
javier <juli99nic@gmail.com>
2020-05-02 21:10:40 -0300

    constantes

---
+++ Fundamentos/constantes.py
@@ -0,0 +1,4 @@
+CONSTANTE= "Valor de la constante"
+
+class Matematicas:
+    PI=3.1416
---
+++ Fundamentos/test_constantes.py
@@ -0,0 +1,10 @@
+from constantes import *
+
+print(CONSTANTE)
+print(Matematicas.PI)
+
+CONSTANTE="El valor de PI abreviado es: "
+Matematicas.PI=3.14
+
+print(CONSTANTE)
+print(Matematicas.PI)
