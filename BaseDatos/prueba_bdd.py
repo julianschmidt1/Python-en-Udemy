4bb46a34c8adc02c1188cbd70fbaa8c2e5a216cf
julianschmidt1 <juli99nic@gmail.com>
2020-05-08 21:17:40 -0300

    Base de datos postresql

---
+++ BaseDatos/prueba_bdd.py
@@ -0,0 +1,15 @@
+import psycopg2
+
+conexion = psycopg2.connect(user='postgres',
+                            password='admin',
+                            host='127.0.0.1',
+                            port='5432',
+                            database='test_db')
+cursor=conexion.cursor()
+sql='SELECT * FROM persona'
+cursor.execute(sql)
+registros=cursor.fetchall()
+print(registros)
+
+cursor.close()
+conexion.close()
